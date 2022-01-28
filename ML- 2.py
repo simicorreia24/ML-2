79e23df3ccc1522cf9b11c8a620cebb352c5b713
Simi Silvester Correia <66421852+simicorreia24@users.noreply.github.com>
2022-01-28 16:58:07 +0530

    Add files via upload

---
+++ ML- 2.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## 1. What is the concept of human learning? Please give two examples.
+
+# In[ ]:
+
+
+# Human Learning concept includes processes like thinking, reasoning, intelligence, etc.
+# eg. we see a cat and attach the term "cat"
+# eg. we react in certain way according to the condition
+
+
+# ## 2. What different forms of human learning are there? Are there any machine learning equivalents?
+
+# In[ ]:
+
+
+# There are three main types of learning: classical conditioning, operant conditioning, and observational learning.
+# Both humans and machines make errors while using their intelligence during problem solving. 
+
+
+# ## 3. What is machine learning, and how does it work? What are the key responsibilities of machine learning?
+
+# In[ ]:
+
+
+# A machine learning (ML) developer is an expert on using data to training models. 
+# The models are then used to automate processes like image classification, speech recognition, and market forecasting
+
+
+# ## 4. Define the terms &quot;penalty&quot; and &quot;reward&quot; in the context of reinforcement learning.
+
+# In[ ]:
+
+
+# In Reinforcement Learning (RL), agents are trained on a reward and punishment mechanism. 
+# The agent is rewarded for correct moves and punished for the wrong ones. 
+# In doing so, the agent tries to minimize wrong moves and maximize the right ones.
+
+
+# ## 5. Explain the term &quot;learning as a search&quot;?
+
+# In[ ]:
+
+
+# Searching through a large space of hypotheses implicitly defined by the hypothesis representation (same for more general learning). 
+# The hypothesis representation defines the space of hypotheses the program can ever represent and therefore can ever learn.
+
+
+# ## 6. What are the various goals of machine learning? What is the relationship between these and human learning?
+
+# In[ ]:
+
+
+# Its goal and usage is to build new and/or leverage existing algorithms to learn from data, in order to build 
+# generalizable models that give accurate predictions, or to find patterns, particularly with new and unseen 
+# similar data. ML and Human learnings are similar both gain knowledge from previous experiences/data.
+
+
+# ## 7. Illustrate the various elements of machine learning using a real-life illustration.
+
+# In[ ]:
+
+
+# These are three types of machine learning: supervised learning, unsupervised learning, and reinforcement learning
+# face recognition, speech recognition
+
+
+# ## 8. Provide an example of the abstraction method.
+
+# In[ ]:
+
+
+# Abstraction is defined as dealing with ideas instead of events. In the context of AI,
+# that means worrying more about what the right algorithm is and less about how to implement it. 
+
+
+# ## 9. What is the concept of generalization? What function does it play in the machine learning process?
+
+# In[ ]:
+
+
+# Generalization refers to your model's ability to adapt properly to new, previously unseen data, drawn from the
+# same distribution as the one used to create the model. Develop intuition about overfitting. Determine whether a
+# model is good or not. Divide a data set into a training set and a test set
+
+
+# ## 10. What is classification, exactly? What are the main distinctions between classification and regression?
+
+# In[ ]:
+
+
+# Classification is the task of predicting a discrete class label. 
+# Regression is the task of predicting a continuous quantity
+
+
+# ## 11. What is regression, and how does it work? Give an example of a real-world problem that was solved using regression.
+
+# In[ ]:
+
+
+# A simple linear regression real life example could mean you finding a relationship between the revenue and 
+# temperature, with a sample size for revenue as the dependent variable. In case of multiple variable regression,
+# you can find the relationship between temperature, pricing and number of workers to the revenue.
+
+
+# ## 12. Describe the clustering mechanism in detail.
+
+# In[ ]:
+
+
+# Clustering is the task of dividing the population or data points into a number of groups such that data points in
+# the same groups are more similar to other data points in the same group than those in other groups.
+# In simple words, the aim is to segregate groups with similar traits and assign them into clusters
+
+
+# ## 13. Make brief observations on two of the following topics:
+# 
+# i. Machine learning algorithms are used
+# ii. Studying under supervision
+# iii. Studying without supervision
+# 
+# iv. Reinforcement learning is a form of learning based on positive reinforcement.
+
+# In[ ]:
+
+
+# Supervised learning- Supervised learning, also known as supervised machine learning, is a subcategory of machine 
+# learning and artificial intelligence. It is defined by its use of labeled datasets to train algorithms that to 
+# classify data or predict outcomes accurately.
+
+
+# Unsupervised learning- Unsupervised learning, also known as unsupervised machine learning, uses machine learning
+# algorithms to analyze and cluster unlabeled datasets. These algorithms discover hidden patterns or data groupings
+# without the need for human intervention.
+
+# Reinforcement learning is a form of learning based on positive reinforcement- Reinforcement learning is a machine
+# learning training method based on rewarding desired behaviors and/or punishing undesired ones. In general, a 
+# reinforcement learning agent is able to perceive and interpret its environment, take actions and learn through 
+# trial and error.
+
